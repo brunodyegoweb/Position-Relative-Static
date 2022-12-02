5272825c6a463c34a60b42ab3cc329dbff812663
Bruno Dyego Silveira da Silva <bruno.ufra@yahoo.com.br>
2022-12-02 12:15:25 -0300

    Manutenção no código

--- js/position.js
+++ js/position.js
@@ -1,13 +1,13 @@
-const $elementExampleFirst = document.querySelector('.square-1')
+const $elementExampleFirst = document.querySelector('.elementExample_first')
 
-const $elementExampleLast = document.querySelector('.square-2')
+const $elementExampleLast = document.querySelector('.elementExample_last')
 
-$elementExampleFirst.addEventListener('click', function(event){
-    this.classList.toggle('elementExample_isActive')
-    $elementExampleLast.classList.add('elementExemple_isActive')
+$elementExampleFirst.addEventListener('click', function(event) {
+    this.classList.toggle('elementExample_isActive')/*se a classe existir ele remove, caso contrario ele adiciona*/ 
+    $elementExampleLast.classList.add('elementExample_isActive')
 })
 
-$elementExampleLast.addEventListener('click', function(element){
+$elementExampleLast.addEventListener('click', function(event) {
     this.classList.toggle('elementExample_isActive')  
-    $elementExampleFirst.classList.add('elementExemple_isActive')
+    $elementExampleFirst.classList.add('elementExample_isActive')
 })
